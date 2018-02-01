9a11634126d047255813df5e058695373c8ac10d
durkesh <durkeshsr@gmail.com>
2018-02-01 18:28:57 +0530

    Create alphabet.py

---
+++ java_beginner/alphabet.py
@@ -0,0 +1,11 @@
+def main():
+    x=input()
+    list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    if x in list:
+        y="Alphabet"
+    else:
+        y="No"
+    print(y)
+if __name__ == '__main__':
+    main()
