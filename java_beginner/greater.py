dc55710b8dc2ed588e8e3ab2011f5c77aff77c2a
durkesh <durkeshsr@gmail.com>
2018-02-01 18:34:57 +0530

    Create greater.py

---
+++ java_beginner/greater.py
@@ -0,0 +1,13 @@
+def main():
+    a=int(input())
+    b=int(input())
+    c=int(input())
+    if a>b and a>c:
+        print(a)
+    elif b>a and  b>c:
+        print(b)
+    else:
+        print(c)
+
+if __name__ == '__main__':
+    main()
